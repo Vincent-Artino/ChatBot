79debd3345684276669b33f7378eebeefada0e50
Vincent Artino <Vincent-Artino@users.noreply.github.com>
2017-05-17 17:32:59 +0530

    Update main.js

--- main.js
+++ main.js
@@ -25,4 +25,96 @@ app.get('/webhook', function(req, res) {
     res.sendStatus(403);          
   }  
 });
+var GetStartedButton = {
+  "get_started":{
+    "payload":"Get started"
+  }
+}
+threadSetUp(GetStartedButton)
+webViewSetUp()
+function receivedMessage(event){
+	var message = event.message;
+	var senderID = event.sender.id;
+	var messageText = message.text;
+	var messageAttachments = message.attachments 
+	console.log(messageAttachments)
+	if(messageText){
+		
+		var attach = {
+                  type:"template",
+                     payload:{
+                        template_type:"button",
+                        text:"Need further assistance? Talk to a representative",
+                        buttons:[
+                           {
+                                "type":"web_url",
+                                "url":"https://the-dream-app.herokuapp.com/",
+                                "title":"View Item",
+                                "webview_height_ratio": "compact
+                           }
+                        ]
+                     }
+                }
+        sendAttachment(attach)
+
+	}
+	else{
+	console.log(event)
+	}
+}
+
+function webViewSetUp(){
+var data = {"whitelisted_domains":[
+    "https://the-dream-app.herokuapp.com/"
+  ]}
+request({
+		uri: 'https://graph.facebook.com/v2.6/me/messenger_profile?access_token='+access_token,
+		method: 'POST',
+		json : data
+	},function (error,response,body){
+		if(!error){
+			console.log(body)
+		}	
+	});
+}
+
+function threadSetUp(messageData){
+request({
+	uri: 'https://graph.facebook.com/v2.6/me/messenger_profile?access_token='+access_token,
+    	method: 'POST',
+    	json: messageData
+	
+},function (error,response,body){
+	if(!error){
+		console.log("set up complete "+JSON.stringify(body));	
+	}	
+});
+}
+
+function sendAttachment(recID,attach){
+	var messageData = {
+	recipient : {
+		id : recID	
+	},
+	message : {
+		attachment :attach
+	}
+}
+	console.log("attaching : "+attach);
+	sendMessage(messageData);
+
+}
+function sendMessage(messageData){
+request({
+	uri: 'https://graph.facebook.com/v2.6/me/messages',
+  	qs: { access_token: access_token },
+    	method: 'POST',
+    	json: messageData
+	
+},function (error,response,body){
+	if(!error){
+		console.log("message sent");	
+	}	
+});
+}
 app.listen(port)
